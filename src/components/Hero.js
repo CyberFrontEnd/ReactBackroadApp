c5921e1f957de441ee562726db349041ed1f91de
CyberFrontEnd <avoelike@gmail.com>
2024-06-22 19:52:17 +0400

    22,06,2024

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,9 @@ const Hero = () => {
     <section className="hero" id="home">
       <div className="hero-banner">
         <h1>continue exploring</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-          explicabo debitis est autem dicta.
-        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          explicabo debitis est autem dicta.</p>
+
         <a href="#tours" className="btn hero-btn">explore tours</a>
       </div>
     </section>
--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from './images/tour-1.jpeg';
+import tour2 from './images/tour-2.jpeg';
+import tour3 from './images/tour-3.jpeg';
+import tour4 from './images/tour-4.jpeg';
+
 export const pageLinks = [
   {id:1,href:'#home', text:'home'},
   {id:2,href:'#about', text:'about'},
@@ -26,3 +31,55 @@ export const services = [
       '              Asperiores, officia.\n' +
       '            '},
 ];
+
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    data: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque\n' +
+      '              vitae tempore voluptatum maxime reprehenderit eum quod\n' +
+      '              exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: '6',
+    cost: 2100
+  },
+  {
+    id: 2,
+    image: tour2,
+    data: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque\n' +
+      '              vitae tempore voluptatum maxime reprehenderit eum quod\n' +
+      '              exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: '6',
+    cost: 2300
+  },
+  {
+    id: 3,
+    image: tour3,
+    data: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque\n' +
+      '              vitae tempore voluptatum maxime reprehenderit eum quod\n' +
+      '              exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: '6',
+    cost: 2400
+  },
+  {
+    id: 4,
+    image: tour4,
+    data: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque\n' +
+      '              vitae tempore voluptatum maxime reprehenderit eum quod\n' +
+      '              exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: '6',
+    cost: 3100
+  }
+]
